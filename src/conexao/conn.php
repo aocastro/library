c99b258c361573418bb1e47c936cd126e658dea8
aocastro <adriano.castro2@etec.sp.gov.br>
2021-06-18 08:52:59 -0300

    Commit de correções e melhorias

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,10 +1,15 @@
 <?php
 
     // Declarar as variáveis necessárias para gerar a minha conexão com o banco de dados ....
-    $hostmane = "fdb22.awardspace.net";
-    $dbname = "2874836_library";
-    $username = "2874836_library";
-    $password = "Et3cL1br@ry";
+    // $hostmane = "fdb22.awardspace.net";
+    // $dbname = "2874836_library";
+    // $username = "2874836_library";
+    // $password = "Et3cL1br@ry";
+   
+    $hostmane = "sql101.epizy.com";
+    $dbname = "epiz_28839334_library";
+    $username = "epiz_28839334";
+    $password = "1lplvcR2bqjtusU";
 
     try {
         $pdo = new PDO('mysql:host='.$hostmane.';dbname='.$dbname, $username, $password);
